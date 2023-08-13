975fd3a021d500dd7c2bf7e4da09c6308c5513d4
João Guedes <joao.guedes.developer@gmail.com>
2023-08-13 12:32:25 -0300

    Atualizando meu portfólio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Typing animation
+var typed = new Typed(".typing", {
+    strings: [  "",
+                "web Designer", 
+                "Programador Python", 
+                "Administrador de Banco de dados", 
+                "Programador Fron End"
+            ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
